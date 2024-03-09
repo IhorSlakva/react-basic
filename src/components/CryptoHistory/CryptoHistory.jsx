7e03bc74a07f3a08912a1cfd234ec70e84255d2e
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-03-09 17:13:41 +0200

    task-4-finished

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,30 @@
 // const tableHeaders = ['№', 'price', 'amount', 'date'];
 
-export const CryptoHistory = () => {
-  return <h2>CryptoHistory</h2>;
+import style from './CryptoHistory.module.css';
+import { dateFormat2 } from 'helpers/dateFormat';
+
+export const CryptoHistory = ({ items }) => {
+  return (
+    <table className={style.table}>
+      <thead className={style.thead}>
+        <tr>
+          <th className={style.th}>№</th>
+          <th className={style.th}>PRICE</th>
+          <th className={style.th}>AMOUNT</th>
+          <th className={style.th}>DATE</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, price, amount, date }, index) => (
+          <tr className={style.tr} key={id}>
+            <td className={style.td}>{index + 1}</td>
+            <td className={style.td}>{price}</td>
+            <td className={style.td}>{amount}</td>
+            <td className={style.td}>{dateFormat2(date)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
--- src/helpers/dateFormat.js
+++ src/helpers/dateFormat.js
@@ -1,5 +1,9 @@
-import { formatDistanceToNow } from 'date-fns';
+import { formatDistanceToNow, format } from 'date-fns';
 
-export const dateFormat = (data) => {
-    return formatDistanceToNow(new Date(data), { addSuffix: true })
-}
+export const dateFormat = data => {
+  return formatDistanceToNow(new Date(data), { addSuffix: true });
+};
+
+export const dateFormat2 = data => {
+  return format(new Date(data), 'Pp');
+};
