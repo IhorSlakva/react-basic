d94880500003e947de0acfb13bae7946c3668ca9
IhorSlakva <igorek.slakva@gmail.com>
2024-03-09 15:19:30 +0200

    first task

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import {
+  Section,
+  Container,
+  BlogCard,
+  Heading,
+  Statistics,
+  CryptoHistory,
+  ForbesList,
+} from 'components';
+import dataArticle from './data/article.json';
+
+export const App = () => {
+  return (
+    <Section>
+      <Container>
+        <Heading title="Task 1 Blog Card" bottom />
+        <BlogCard
+          poster={dataArticle.poster}
+          tag={dataArticle.tag}
+          title={dataArticle.title}
+          description={dataArticle.description}
+          name={dataArticle.name}
+          avatar={dataArticle.avatar}
+          postedAt={dataArticle.postedAt}
+        />
+
+        <Heading title="Task 2 Statistics" top bottom />
+        <Statistics title="Main Statistics" />
+
+        <Heading title="Task 3 Forbes list" top bottom />
+        <ForbesList />
+
+        <Heading title="Task 4 Crypto history" top bottom />
+        <CryptoHistory />
+      </Container>
+    </Section>
+  );
+};
---
+++ src/components/BlogCard/BlogCard.jsx
@@ -0,0 +1,34 @@
+import styles from './BlogCard.module.css';
+import { dateFormat } from 'helpers/dateFormat';
+
+export const BlogCard = ({
+  poster,
+  tag,
+  title,
+  description,
+  name,
+  avatar,
+  postedAt,
+}) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.cardHeader}>
+        <img className={styles.cardPoster} src={poster} alt={description} />
+      </div>
+      <div className={styles.cardBody}>
+        <span className={styles.tag}>{tag}</span>
+        <h2 className={styles.cardTitle}>{title}</h2>
+        <p className={styles.cardText}>{description}</p>
+      </div>
+      <div className={styles.cardFooter}>
+        <div className={styles.userBox}>
+          <img className={styles.avatar} src={avatar} alt={name} />
+          <div>
+            <h3 className={styles.userName}>{name}</h3>
+            <small className={styles.date}>{dateFormat(postedAt)}</small>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
